0b5e7d2079c2f6439e90983dd94b158d8b169d95
iZeColdeBlue <schnurrerjakob@gmail.com>
2025-01-11 12:20:53 +0100

    Test Kinect Depth Sensor
    Not real game controlls yet, just testing how to get sensor data into unity

---
+++ Assets/Scripts/MeasureDepth.cs
@@ -0,0 +1,147 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Windows.Kinect;
+
+public class MeasureDepth : MonoBehaviour
+{
+
+    public MultiSourceManager mMultiSource;
+    public Texture2D mDepthTexture;
+
+    //Cutoffs
+    [Range(0,1.0f)]
+    public float mDepthSensitivity = 1;
+
+    [Range(10, 10f)]
+    public float mWallDepth = -10;
+
+    [Header("Top & Bottom")]
+    [Range(-1   , 1f)]
+    public float mTopCutOff = 1;
+    [Range(-1, 1f)]
+    public float mBottomCutOff = -1;
+
+    [Header("Left & Right")]
+    [Range(-1, 1f)]
+    public float mLeftCutOff = -1;
+    [Range(-1, 1f)]
+    public float mRightCutOff = 1;
+
+    //Depth
+    private ushort[] mDepthData = null;
+    private CameraSpacePoint[] mCameraSpacePoints = null;
+    private ColorSpacePoint[] mColorSpacePoints = null;
+    private List<ValidPoint> mValidPoints = null;
+
+    //Kinect
+    private KinectSensor mSensor = null;
+    private CoordinateMapper mMapper = null;
+
+    private readonly Vector2Int mDepthResolution = new Vector2Int(512, 424);
+
+    private void Awake()
+    {
+        mSensor = KinectSensor.GetDefault();
+        mMapper = mSensor.CoordinateMapper;
+
+        int arraySize = (int)mDepthResolution.x * (int)mDepthResolution.y;
+
+        mCameraSpacePoints = new CameraSpacePoint[arraySize];
+        mColorSpacePoints = new ColorSpacePoint[arraySize];
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            mValidPoints=DepthToColor();
+
+            mDepthTexture = CreateTexture(mValidPoints);
+        }
+    }
+
+    private List<ValidPoint> DepthToColor()
+    { 
+        //Points to return
+        List<ValidPoint> validPoints = new List<ValidPoint>();
+
+        // Get depth
+        mDepthData = mMultiSource.GetDepthData();
+        
+        //Map
+        mMapper.MapDepthFrameToCameraSpace(mDepthData, mCameraSpacePoints);
+        mMapper.MapDepthFrameToColorSpace(mDepthData, mColorSpacePoints);
+
+        //Filter
+        for(int i=0; i<mDepthResolution.x/8; i++)
+        {
+            for(int j =0; j<mDepthResolution.y/8; j++)
+            {   
+                //Sample Index - reducing the number of tested DepthPoints
+                int sampleIndex = (j*mDepthResolution.x) + i;
+                sampleIndex *= 8;
+
+                if (mCameraSpacePoints[sampleIndex].X < mLeftCutOff)
+                    continue;
+
+                if (mCameraSpacePoints[sampleIndex].X > mRightCutOff)
+                    continue;
+
+                if (mCameraSpacePoints[sampleIndex].Y < mTopCutOff)
+                    continue;
+
+                if (mCameraSpacePoints[sampleIndex].Y > mBottomCutOff)
+                    continue;
+
+                //create a new valid point
+                ValidPoint newPoint = new ValidPoint(mColorSpacePoints[sampleIndex], mCameraSpacePoints[sampleIndex].Z);
+
+                //test depth
+                if (mCameraSpacePoints[sampleIndex].Z >= mWallDepth)
+                    newPoint.mWithinWallDepth = true;
+
+                //add valid points to List
+                validPoints.Add(newPoint); 
+            }
+        }
+
+        return validPoints;
+    }
+
+    private Texture2D CreateTexture(List<ValidPoint> validPoints)
+    {
+        Texture2D newTexture = new Texture2D(1920, 1080, TextureFormat.Alpha8, false);
+
+        for(int x=0; x <1920; x++)
+        {
+            for (int y = 0; y < 1080; y++)
+            {
+                newTexture.SetPixel(x, y, Color.clear);
+            }
+        }
+
+        foreach(ValidPoint point in validPoints)
+        {
+            newTexture.SetPixel((int)point.colorSpace.X, (int)point.colorSpace.Y, Color.black);
+        }
+
+        newTexture.Apply();
+
+        return newTexture;
+    }
+
+}
+
+public class ValidPoint
+{
+    public ColorSpacePoint colorSpace;
+    public float z = 0.0f;
+
+    public bool mWithinWallDepth = false;
+
+    public ValidPoint(ColorSpacePoint newColorSpace, float newZ)
+    {
+        colorSpace = newColorSpace;
+        z = newZ;
+    }
+}
