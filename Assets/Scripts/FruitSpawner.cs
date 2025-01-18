45db949c498495ef4b382f78e3c7f50be905df4a
iZeColdeBlue <schnurrerjakob@gmail.com>
2025-01-18 18:05:38 +0100

    Add controlls, scoring &  spawning of collectable objects

--- Assets/Scripts/FruitSpawner.cs
+++ Assets/Scripts/FruitSpawner.cs
@@ -5,69 +5,80 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-
     public GameObject[] Fruits;
     public GameObject[] Sweets;
 
     public float spawnPeriod = 10;
+    public float fallSpeed;
+    public int counter;
 
-    private int maxNumObjects = 10;
-    private int currentNumObjects = 10;
-
-
+    private int maxNumObjects = 5;
+    private int currentNumObjects = 0;
+    private PlayerController playerController;
+    private bool hasIncreased = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        /*
-        for(int i=0; i<maxNumObjects; i++)
-        { 
-            SpawnFruit();
-        }
-        */
+        playerController = FindObjectOfType<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
         //counts all objects that are catchable
-        GameObject[] allObjects = GameObject.FindGameObjectsWithTag("catchableObject");
+        GameObject[] catchableFruits = GameObject.FindGameObjectsWithTag("catchableFruit");
+        GameObject[] catchableSweets = GameObject.FindGameObjectsWithTag("catchableSweet");
+        GameObject[] allObjects = new GameObject[catchableFruits.Length + catchableSweets.Length];
+        catchableFruits.CopyTo(allObjects, 0);
+        catchableSweets.CopyTo(allObjects, catchableFruits.Length);
         currentNumObjects = allObjects.Length;
         Debug.Log(currentNumObjects);
 
         if (currentNumObjects < maxNumObjects)
         {
             //decides if spawned Object is a fruit or a sweet
-            int randSelect = Random.Range(1,4);
-            if(randSelect == 1) 
-            { 
-                SpawnSweet(); 
-            } else { 
-                SpawnFruit(); 
+            int randSelect = Random.Range(1, 4);
+            if (randSelect == 1)
+            {
+                SpawnSweet();
             }
+            else
+            {
+                SpawnFruit();
+            }
+        }
 
+        // Increase maxNumObjects and fallSpeed only if counter is divisible by 10 and maxNumObjects is less than a certain limit
+        if (playerController.counter % 10 == 0 && playerController.counter != 0 && !hasIncreased && maxNumObjects <= 10)
+        {
+            maxNumObjects++;
+            fallSpeed += 10f;
+            hasIncreased = true; // Setze die Variable auf true, um anzuzeigen, dass die Erhöhung stattgefunden hat
+        }
+        else if (playerController.counter % 10 != 0)
+        {
+            hasIncreased = false; // Setze die Variable zurück, wenn der counter nicht mehr durch 10 teilbar ist
         }
     }
 
     void SpawnFruit()
     {
-        Debug.Log("Spawning a fruit!");
         GameObject newFruit = Fruits[Random.Range(0, Fruits.Length)]; //selects a random fruit from the array
 
         //spawn position is randomized & tag is assigned
         Vector3 spawnPosition = new Vector3(Random.Range(-4f, 4f), 10f, Random.Range(-4f, 4f));
         GameObject instantiatedFruit = Instantiate(newFruit, spawnPosition, Quaternion.identity);
-        instantiatedFruit.tag = "catchableObject";
+        instantiatedFruit.tag = "catchableFruit";
     }
 
     void SpawnSweet()
     {
-        Debug.Log("Spawning a sweet!");
         GameObject newSweet = Sweets[Random.Range(0, Sweets.Length)]; //selects a random sweet from the array
 
         //spawn position is randomized & tag is assigned
         Vector3 spawnPosition = new Vector3(Random.Range(-4f, 4f), 10f, Random.Range(-4f, 4f));
         GameObject instantiatedSweet = Instantiate(newSweet, spawnPosition, Quaternion.identity);
-        instantiatedSweet.tag = "catchableObject";
+        instantiatedSweet.tag = "catchableSweet";
     }
 }
--- Assets/Scripts/Old/FallingCube.cs
+++ Assets/Scripts/Old/FallingCube.cs
@@ -2,28 +2,60 @@ using UnityEngine;
 
 public class FallingCube : MonoBehaviour
 {
-    private float fallSpeed = 3f; //make this random so Objects fall at different speeds
+    public float fallSpeed = 10f; //make this random so Objects fall at different speeds
     //would be cool, if they would spin
 
+    private PlayerController playerController;
+
+    private Vector3 spinSpeed;
+
+    void Start()
+    {
+        // Ensure the collider is set as a trigger
+        Collider collider = GetComponent<Collider>();
+        if (collider != null)
+        {
+            collider.isTrigger = true;
+        }
+
+        // Find the PlayerController in the scene
+        playerController = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
+
+        spinSpeed = new Vector3(0, 90f, 0);
+    }
 
     void Update()
     {
         transform.Translate(Vector3.down * fallSpeed * Time.deltaTime);
+
+        transform.Rotate(spinSpeed * Time.deltaTime);
 
         if (transform.position.y < -1f) // if object is below -1f(ground) destroy it
         {
             Destroy(gameObject);
         }
     }
-    void OnTriggerEnter(Collider other)
-{
-    if (CompareTag("FallingCube"))
-    {
-        //if player catches the falling cube, destroy the cube
-        Debug.Log("Player touched FallingCube. Destroying the cube.");
-        Destroy(gameObject); //destrys falling cube
-    }
 
-}
-   
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //if player catches the falling cube, destroy the cube
+            Debug.Log("Player touched FallingCube. Destroying the cube.");
+            Destroy(gameObject); //destrys falling cube
+
+            // Call IncreaseScore function in PlayerController
+            if (playerController != null)
+            {
+                playerController.IncreaseScore();
+            }
+        }
+
+        if (other.CompareTag("Environment"))
+        {
+            //if player catches the falling cube, destroy the cube
+            //Debug.Log("Player touched FallingCube. Destroying the cube.");
+            Destroy(gameObject); //destrys falling cube
+        }
+    }
 }
